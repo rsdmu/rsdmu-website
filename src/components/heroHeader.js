1bbb2a194748fe0596676a7634ce4d616c0894c1
rsdmu <rashid.ahmad.mushkani@umontreal.ca>
2024-11-13 14:04:42 -0500

    x

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { StaticQuery, graphql, Link } from "gatsby"
-
 export default () => (
   <StaticQuery
     query={graphql`
@@ -22,7 +21,7 @@ export default () => (
           className="primary-content" 
           dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
         />
-        <Link to='/contact' className="button -primary">Get in touch &rarr;</Link> 
+        <Link to='/contact' className="button -primary">Get in touch &rarr;</Link>
       </div>
     )}
   />
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,7 @@
 import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
-import Navigation from "./navigation" // Assuming Navigation is also in the same components directory
-import SEO from "./seo" // Importing SEO
-import 'prismjs/themes/prism-okaidia.css'
-import "../styles/global.scss";
-
+import Navigation from "../components/navigation"
+import 'prismjs/themes/prism-okaidia.css';
 
 export default ({ children }) => {
   const data = useStaticQuery(
@@ -18,33 +15,17 @@ export default ({ children }) => {
       }
     `
   )
-
   return (
     <div className="site-wrapper">
-      <SEO 
-        title={data.site.siteMetadata.title} 
-        description="rsdmu" // Website description
-        metaTag="l3J0y3XD8D3n9javX6yPcBrEYB9SXnICRguNy3C5JHA" // Google Search Console verification code
-      />
       <header className="site-header">
-        <div className="background-image">
-          <div className="centered-content">
-            <h1 className="site-title">
-              <Link to="/">{data.site.siteMetadata.title}</Link>
-            </h1>
-            <nav className="main-menu">
-              <Link to="/">Home</Link>
-              <Link to="/bio">Bio</Link>
-              <Link to="/projects">Projects</Link>
-              <Link to="/books">Books</Link>
-              <Link to="/contact">Contact</Link>
-            </nav>
-          </div>
+        <div className="site-title">
+          <Link to="/">{data.site.siteMetadata.title}</Link>
         </div>
+        <Navigation />
       </header>
-      <main>{children}</main>
+      {children}
       <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()} Rashid A. Mushkani</p>
+        <p>&copy; {new Date().getFullYear()} Delog &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://w3layouts.com">W3Layouts</a></p>
       </footer>
     </div>
   )
