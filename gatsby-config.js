e2cf1409c6e4dd2eb8840c20e95e1e96ddd646be
rsdmu <rashid.ahmad.mushkani@umontreal.ca>
2024-11-14 00:35:50 -0500

    c

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "assets",
-        path: `${__dirname}/static/admin/assets`,
+        path: `${__dirname}/static/assets`,
       },
     },
 
